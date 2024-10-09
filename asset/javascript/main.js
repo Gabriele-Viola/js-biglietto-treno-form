c861d0b5b486834f66d449dc100ad82aa0fc7c1c
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-10-09 17:22:49 +0200

    add const input js

--- asset/javascript/main.js
+++ asset/javascript/main.js
@@ -1,26 +1,58 @@
-let kilometres;
-let age;
-const price_at_kilometres = 0.21;
-let under_18 = price_at_kilometres * 80 / 100;
-let over_65 = price_at_kilometres * 60 / 100;
-let your_ticket;
-let message;
+const formEl = document.querySelector('form')
+const bigliettoel = document.querySelector('biglietto')
+console.log(formEl);
+
+
+formEl.addEventListener('submit', function (e) {
+    e.preventDefault()
+
+
+
+    const travelerName = e.target.traveler.value
+    const kilometres = e.target.km.value;
+    const age = e.target.age.value
+
+    const price_at_kilometres = 0.21;
+    let under_18 = price_at_kilometres * 80 / 100;
+    let over_65 = price_at_kilometres * 60 / 100;
+    let your_ticket;
+
+    const yourData =
+    `        <h1 class="yourname">${travelerName}</h1>
+        <div class="offerta">${age}</div>
+        <div class="kmPercorsi">${kilometres}</div>
+        <div class="costo">${your_ticket}</div>
+        `
+
+    bigliettoel.insertAdjacentElement('beforeend', yourData)
+
+    if (age >= 18 && age < 65) {
+        your_ticket = kilometres * under_18
+        
+    } else if (age < 18) {
+        your_ticket = kilometres * price_at_kilometres
+
+    } else {
+        your_ticket = kilometres * over_65
+    }
+    console.log(message);
+
+
+
+
+
+
+
+})
+
+
+
+
+
 
 //Raccogliere dati
 
-kilometres = Number(prompt('How many kilometres do you want to travel?'));
-age = Number(prompt('How old are you?'));
+// kilometres = Number(prompt('How many kilometres do you want to travel?'));
+// age = Number(prompt('How old are you?'));
 
 // elaborazione dati
-
-if ( age < 18 ){
-    your_ticket = kilometres * under_18
-    message = `You have ${age} years old, so you are under 18 and you will have to travel ${kilometres} kilometres.Your Ticket will cost ${your_ticket.toFixed(2)}`
-} else if ( age >= 18 && age < 65 ){
-    your_ticket = kilometres * price_at_kilometres
-        message = `You have ${age} years old and you will have to travel ${kilometres} kilometres.Your Ticket will cost ${your_ticket.toFixed(2)}`
-}else{
-    your_ticket = kilometres * over_65
-    message = `You have ${age} years old, so you are over 65 and you will have to travel ${kilometres} kilometres.Your Ticket will cost ${your_ticket.toFixed(2)}`
-}
-console.log(message);
